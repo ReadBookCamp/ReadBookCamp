67aff80d962bb1e5296c728e04bc5ba38edd5f51
dsx <876337269@qq.com>
2020-06-27 00:00:00 +0800

    ci(gihub): 增加action

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@
 const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  assetPrefix: !debug ? 'https://readbookcamp.github.io/ReadBookCamp/' : '',
   /* webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
