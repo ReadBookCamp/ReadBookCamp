a1cca63ca23afa8b1acf66be2f40fbafdc3bf28e
dsx <876337269@qq.com>
2020-06-27 18:19:28 +0800

    feat(log): 添加日志

--- pages/api/github/join.ts
+++ pages/api/github/join.ts
@@ -5,6 +5,7 @@ import cookie from 'cookie';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { gh_user_info } = cookie.parse(req.headers.cookie || '');
   const { login: name } = JSON.parse(gh_user_info);
+  console.log('LEARN_GH_TOKEN', process.env.LEARN_GH_TOKEN);
   const octokit = new Octokit({
     auth: process.env.LEARN_GH_TOKEN,
     userAgent: 'ReadBookCamp',
