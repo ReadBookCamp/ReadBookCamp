61619c6535af6da4c6f004040dc97f167d49527d
dsx <876337269@qq.com>
2020-06-27 18:23:34 +0800

    fix:添加日志

--- pages/api/members/list.ts
+++ pages/api/members/list.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-unfetch';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log('LEARN_GH_TOKEN', process.env.LEARN_GH_TOKEN);
   const data = await fetch(
     'https://raw.githubusercontent.com/ReadBookCamp/ReadBookCamp/master/data/latest.json',
   ).then((res) => res.json());
